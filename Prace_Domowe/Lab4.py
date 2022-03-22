f32aa77c9e8afc2c0ea17ae71ad5a83e26f4edcf
brozanski <100760688+brozanski@users.noreply.github.com>
2022-03-22 16:12:50 +0100

    Create Lab4.py

---
+++ Prace_Domowe/Lab4.py
@@ -0,0 +1,27 @@
+import sys
+import math
+#PRACA DOMOWA LAB4
+
+# #Zad.1
+# liczby=[]
+# for x in range(0,31):
+#     liczby.append(x * 2)
+# file = open("pomnozone.txt", "w+")
+# file.write(str(liczby))
+# file.close()
+
+# #Zad.2
+# with open('pomnozone.txt', 'r') as plik:
+#     for linia in plik:
+#         print(linia, end='')
+
+# #Zad.3
+# print("Wpisz kilka linijek tekstu")
+# linijki = sys.stdin.readline()
+# plik = open('linijki.txt', 'w+')
+# plik.write(linijki)
+# plik.close()
+#
+# with open('linijki.txt', 'r') as plik:
+#     for linia in plik:
+#         print(linia, end='')
