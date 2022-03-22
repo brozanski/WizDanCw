0fe05f007e26481a4b85525eb495feb3078d54b4
brozanski <100760688+brozanski@users.noreply.github.com>
2022-03-22 17:23:25 +0100

    Update Lab3.py

--- Prace_Domowe/Lab3.py
+++ Prace_Domowe/Lab3.py
@@ -1,1 +1,34 @@
+import sys
+import math
+import random
+# #Zad. 1
+# a = []
+# for x in range(1,11):
+#     a.append(1-x)
+# print(a)
+#
+# b = []
+# for y in range(8):
+#     b.append(4**y)
+# print(b)
+#
+# c = []
+# for z in b:
+#     if z % 2 == 0:
+#         c.append(z)
+# print(c)
 
+# #Zad. 2
+# lista1 = []
+# i=1
+# while i<11:
+#     x = (random.randint(0, 30))
+#     lista1.append(x)
+#     i+=1
+# print(lista1)
+#
+# lista2 = []
+# for y in lista1:
+#     if y % 2 == 0:
+#         lista2.append(y)
+# print(lista2)
