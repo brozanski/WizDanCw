5b383f8f46333d4d0727d602a5141779a34d636b
brozanski <100760688+brozanski@users.noreply.github.com>
2022-03-22 16:11:54 +0100

    Create Lab1.py

---
+++ Prace_Domowe/Lab1.py
@@ -0,0 +1,69 @@
+import sys
+import math
+#Zadania domowe Lab1
+
+# #Zad.1
+# a = "string 1"
+# b = "string 2"
+# c = 13
+# d = 12
+# e = 1.2
+# f = 6.41
+# g = 5+2j
+# h = 8+4j
+# print(a,b,c,d,e,f,g,h)
+
+# #Zad.2
+# a = input("Podaj pierwsza liczbe: ")
+# oper = input("Jakie dzialanie chcesz wykonac? ")
+# b = input("Podaj druga liczbe: ")
+# a = int(a)
+# b = int(b)
+# if oper=='+':
+#     wynik = a+b
+# elif oper=='-':
+#     wynik = a-b
+# elif oper=='*':
+#     wynik = a*b
+# elif oper=='/':
+#     wynik = a/b
+# print(wynik)
+
+# #Zad.3
+
+# #Zad.4
+# a = math.e**10
+# b = (math.pow(math.log(5+math.pow(math.sin(8), 2)), 1/6))
+# c = math.floor(3.55)
+# d = math.ceil(4.80)
+# print(a,b,c,d)
+
+# #Zad.5
+# imie = "BOGUMIŁ"
+# nazwisko = "RÓŻAŃSKI"
+#
+# imie = imie.capitalize()
+# nazwisko = nazwisko.capitalize()
+# print(imie, nazwisko)
+
+# #Zad.6
+# tekst = "la la baba ka la la"
+# policz = tekst.count("la")
+# print(policz)
+
+# #Zad.7
+# a = "To jest łańcuch"
+# print(a[1],a[-1])
+
+# #Zad.8
+# tekst = "la la baba ka la la"
+# podziel = tekst.split()
+# print(podziel)
+
+# #Zad.9
+# str = "Format"
+# fl = 2.22
+# print(str, fl)
+# print(hex(0XFF))
+# szesnstkowa = 0x1f
+# print('{0:x}'.format(szesnstkowa))
