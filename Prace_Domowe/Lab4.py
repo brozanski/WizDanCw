4e5b2a82400d9e42a92381ce8dda743291b87e08
brozanski <100760688+brozanski@users.noreply.github.com>
2022-03-23 09:49:43 +0100

    Update Lab4.py

--- Prace_Domowe/Lab4.py
+++ Prace_Domowe/Lab4.py
@@ -52,3 +52,33 @@ import math
 # zakup.ile_produktu()
 # zakup.ile_kosztuje()
 
+# #Zad. 6
+# class Robaczek:
+#     x = 0
+#     y = 0
+#     krok = 1
+# 
+#     def __init__(self, x, y, krok):
+#         self.x = x
+#         self.y = y
+#         self.krok = krok
+# 
+#     def idz_w_gore(self, ile_krokow):
+#         self.y += (ile_krokow * self.krok)
+#     def idz_w_dol(self, ile_krokow):
+#         self.y -= (ile_krokow * self.krok)
+#     def idz_w_prawo(self, ile_krokow):
+#         self.x += (ile_krokow * self.krok)
+#     def idz_w_lewo(self, ile_krokow):
+#         self.y -= (ile_krokow * self.krok)
+# 
+#     def pokaz_gdzie_jestes(self):
+#         print("Robaczek znajduje się na współrzędnych x = " + str(self.x) + " i y =" + str(self.y))
+# 
+# ruch = Robaczek(0, 0, 1) #pozycja startowa i wielkosc kroku robaczka
+# 
+# ruch.idz_w_prawo(3)
+# ruch.idz_w_lewo(7)
+# ruch.idz_w_gore(1)
+# ruch.idz_w_dol(2)
+# ruch.pokaz_gdzie_jestes()
