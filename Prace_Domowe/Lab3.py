9cec7f9394a653fcdc0b77d0ec3a4642d16258e0
brozanski <100760688+brozanski@users.noreply.github.com>
2022-03-23 08:55:45 +0100

    Update Lab3.py

--- Prace_Domowe/Lab3.py
+++ Prace_Domowe/Lab3.py
@@ -32,3 +32,42 @@ import random
 #     if y % 2 == 0:
 #         lista2.append(y)
 # print(lista2)
+
+# #Zad. 3
+# slownik = {'szt.' : ["Mango", "Pączek", "Bluza"],
+#            'litr' : ["Pepsi","Olej"],
+#            'kg' : ["Mąka","Banany"]}
+# # print(slownik)
+# # sztuki = []
+# # for key in slownik.items():
+# #     if key == 'szt.'
+# #         sztuki.append(values)
+# sztuki =  [x for x in slownik if slownik[x]=='szt.']
+# print(sztuki)
+
+# #Zad. 4
+# def trojkat_prostokatny(a,b,c):
+#     wzor = (a ** 2 + b ** 2 == c ** 2) or (a ** 2 + c ** 2 == b ** 2) or (b ** 2 + c ** 2 == a ** 2)
+#
+#     if wzor:
+#         print("Trojkąt jest prostokątny!")
+#         return 1
+#     else:
+#         print("Trojkąt nie jest prostokątny!")
+#         return 0
+# print(trojkat_prostokatny(3,4,5))
+# print(trojkat_prostokatny(5,4,3))
+# print(trojkat_prostokatny(4,6,8))
+
+# #Zad. 5
+# def pole_trapezu (a,b,h):
+#     pole = ((a+b)*h)/2
+#     return pole
+# print(pole_trapezu(4,6,8))
+
+# #Zad. 6
+# def iloczyn(a=1,b=4,ile=10):
+#   for i in range (ile):
+#     a*=b
+#   return a
+# print(iloczyn())
